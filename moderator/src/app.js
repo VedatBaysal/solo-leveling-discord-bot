a926305602692b44d23993998693642150e6f452
vedatbaysal <vedatbaysall@gmail.com>
2021-05-15 19:38:00 +0300

    Added rock paper scissors game && sending message when a special member is online :)

---
+++ moderator/src/app.js
@@ -0,0 +1,38 @@
+import { rockPaperScissors } from './commands/rock-paper-scissors.js'
+import dotenv from 'dotenv'
+dotenv.config()
+import dc from 'discord.js'
+const client = new dc.Client()
+
+const prefix = '!'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', (msg) => {
+  if (!msg.content.startsWith(prefix)) return
+  let comment = msg.content.slice(1, msg.content.length)
+  if (comment === 'rock' || comment === 'paper' || comment === 'scissors') {
+    let result = rockPaperScissors(comment)
+    console.log(result)
+    msg.reply(result)
+  }
+  if (comment === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+  let member = newPresence.member
+  if (member.id === process.env.SPECIAL_MEMBER) {
+    if (oldPresence.status !== newPresence.status) {
+      if (newPresence.status === 'online') {
+        const user = client.users.cache.get(process.env.ADMIN)
+        user.send(`${process.env.SPECIAL_MEMBER_NAME} Online`)
+      }
+    }
+  }
+})
+
+client.login(process.env.TOKEN)
---
+++ moderator/src/commands/rock-paper-scissors.js
@@ -0,0 +1,21 @@
+const rockPaperScissors = (x) => {
+  let arr = ['rock', 'paper', 'scissors']
+  if (x !== 'rock' && x !== 'paper' && x !== 'scissors')
+    return 'Unexpected choose'
+  let rnd = Math.random(0, 3)
+  switch (x) {
+    case 'rock':
+      if (arr[rnd] === 'rock') return 'ROCK: Draw'
+      else if (arr[rnd] === 'paper') return 'PAPER: Computer win'
+      else return 'SCISSORS: you win'
+    case 'paper':
+      if (arr[rnd] === 'paper') return 'PAPER: Draw'
+      else if (arr[rnd] === 'scissors') return 'SCISSORS: Computer win'
+      else return 'ROCK: you win'
+    case 'scissors':
+      if (arr[rnd] === 'scissors') return 'SCISSORS: Draw'
+      else if (arr[rnd] === 'rock') return 'ROCK: Computer win'
+      else return 'PAPER: you win'
+  }
+}
+export { rockPaperScissors }
